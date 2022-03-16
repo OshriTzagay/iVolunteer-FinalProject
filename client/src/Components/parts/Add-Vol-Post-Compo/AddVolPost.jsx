8543ffedb143d986cd56f3261e9e47d4826d4cb2
Lior-Yosef <lioryosef06@gmail.com>
2022-03-16 21:20:15 +0200

    add style css to login and change inpout on forn addVolPost

--- client/src/Components/parts/Add-Vol-Post-Compo/AddVolPost.jsx
+++ client/src/Components/parts/Add-Vol-Post-Compo/AddVolPost.jsx
@@ -1,5 +1,8 @@
 import { AddPost } from "../../../Services/volPosts-service";
 import { useState } from "react";
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import "./Add-style.css";
 
 export const AddVolPost = () => {
   const [post, setPost] = useState({});
@@ -15,99 +18,41 @@ export const AddVolPost = () => {
     AddPost(post);
   };
 
+
   return (
     <div>
-      <h1>add post</h1>
-      <form>
-        <label>First name </label>
-        <input
-          type="text"
-          onChange={ChangingTheValue}
-          name="FirstName"
-          placeholder="Enter your first name"
-        />
+      <h1>Add post</h1>
 
-        <label>Last name </label>
-        <input
-          type="text"
-          onChange={ChangingTheValue}
-          name="LastName"
-          placeholder="Enter your last name"
-        />
-        <label> Email </label>
-        <input
-          type="text"
-          onChange={ChangingTheValue}
-          name="Email"
-          placeholder="Enter your Email"
-        />
-        <label> Age </label>
-        <input
-          type="number"
-          onChange={ChangingTheValue}
-          name="Age"
-          placeholder="Enter your Age"
-        />
-        <label> Skills </label>
-        <input
-          type="text"
-          onChange={ChangingTheValue}
-          name="Skills"
-          placeholder="Enter any spacial skills"
-        />
-        <label> Language</label>
-        <input
-          type="text"
-          onChange={ChangingTheValue}
-          name="Language"
-          placeholder="Enter any Language you speak"
-        />
-        <label> Skills </label>
-        <input
-          type="text"
-          onChange={ChangingTheValue}
-          name="Skills"
-          placeholder="Enter any spacial skills"
-        />
-        <label> City </label>
-        <input
-          type="text"
-          onChange={ChangingTheValue}
-          name="City"
-          placeholder="Enter the city you want to volunteer in "
-        />
-        <label> StartHour </label>
-        <input
-          type="time"
-          onChange={ChangingTheValue}
-          name="StartHour"
-          placeholder="Enter the hour you can start in "
-        />
-        <label> FinishHour </label>
-        <input
-          type="time"
-          onChange={ChangingTheValue}
-          name="FinishHour"
-          placeholder="Enter the hour you want finish volunteer in "
-        />
-        <label> Description </label>
-        <input
-          type="text"
-          onChange={ChangingTheValue}
-          name="Description"
-          placeholder="write about yourself"
-        />
-        <label> Phone </label>
-        <input
-          type="tel"
-          onChange={ChangingTheValue}
-          name="Phone"
-          placeholder="enter your phone number"
-        />
+      <form className="form" onSubmit={SendThePost}>
+        <div className="inputs">
+          <TextField label=" First name" placeholder="Enter your first name" name="FirstName" type="text" onChange={ChangingTheValue} multiline required />
+          <TextField label="Last name" placeholder="Enter your Last name" name="LastName" type="text" onChange={ChangingTheValue} multiline required />
+        </div>
 
-        <button type="submit" onClick={SendThePost}>
-          click
-        </button>
+        <div className="inputs">
+          <TextField label="Email" placeholder="Enter your Email" name="Email" type="text" onChange={ChangingTheValue} multiline required   />
+          <TextField label="Age" placeholder="Enter your Age" name="Age" type="number" onChange={ChangingTheValue} multiline />
+        </div>
+
+        <div>
+          <TextField label="Skills" placeholder="Enter any spacial skills" name="Skills" type="text" onChange={ChangingTheValue} multiline />
+          <TextField label="Language" placeholder="Enter your Language" name="Language" type="text" onChange={ChangingTheValue} multiline />
+        </div>
+
+        <div>
+          <TextField label="City" placeholder="Enter your city" name="City" type="text" onChange={ChangingTheValue} multiline />
+          <TextField label="StartHour" placeholder="example: 1500" name="StartHour" type="number" onChange={ChangingTheValue} multiline />
+        </div>
+
+        <div>
+          <TextField label="FinishHour" placeholder="example: 1900 " name="FinishHour" type="number" onChange={ChangingTheValue} multiline />
+          <TextField label="Phone" placeholder="enter a phone number" name="Phone" type="tel" onChange={ChangingTheValue} multiline />
+        </div>
+
+        <TextField label="Description" placeholder="write about yourself and your Skills" name="Description" type="text" onChange={ChangingTheValue} multiline />
+
+        {/* <Button variant="contained" component="span" type="submit">Send</Button> */}
+        <button type="submit" >click</button>
       </form>
     </div>
   );
