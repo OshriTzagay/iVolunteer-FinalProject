f22aeca180fb9385c797733f09d9235e99ae4060
matan-ysayas <matan.ysayas@gmail.com>
2022-03-23 18:39:14 +0200

    d

--- SERVER/server.js
+++ SERVER/server.js
@@ -43,8 +43,8 @@ app.listen(port, () => {
 
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/")));
-  app.get("/", (req, res) => {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+  app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../client/build", "index.html"));
   });
 }
