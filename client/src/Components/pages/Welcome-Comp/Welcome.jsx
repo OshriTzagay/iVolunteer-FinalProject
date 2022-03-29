eb17bacd4bed70df4f559966d0549987bdce8300
matan-ysayas <matan.ysayas@gmail.com>
2022-03-29 23:09:14 +0300

    add shit

--- client/src/Components/pages/Welcome-Comp/Welcome.jsx
+++ client/src/Components/pages/Welcome-Comp/Welcome.jsx
@@ -16,54 +16,37 @@ function Welcome() {
   <div className="Welcome-Container">
   <div className="meet-us">
     <div className="cards">
-
-      <img src="https://cdn-icons-png.flaticon.com/512/1759/1759366.png" alt="" />
-    
+      <img src="/Media/lior.jpeg" alt="" />
       <h2>Lior Yosef</h2>
       <div className="contact-icons">
-      <p><GitHubIcon className="icon"/></p>
-     <p><FacebookRoundedIcon className="icon"/></p> 
-    <p><AlternateEmailRoundedIcon className="icon"/></p> 
-
+      <a href="https://github.com/Lior-Yosef"><i className="fab fa-github"></i></a>
+     <a href="https://www.linkedin.com/in/lior-yosef-6314b0225/"><i className="fab fa-linkedin"></i></a> 
+    <a href="mailto:Lioryosef06@gmail.com"><i className="fas fa-at"></i></a> 
       </div>
-
-      
     </div>
     <div className="cards">
-
-      <img src="https://cdn-icons-png.flaticon.com/512/1759/1759366.png" alt="" />
-    
+      <img src="/Media/oshri.jpg" alt="" />
       <h2>Oshri-el Tzagay</h2>
       <div className="contact-icons">
-      <p><GitHubIcon className="icon"/></p>
-     <p><FacebookRoundedIcon className="icon"/></p> 
-    <p><AlternateEmailRoundedIcon className="icon"/></p> 
-
+      <a href="https://github.com/OshriTzagay"><i className="fab fa-github"></i></a>
+      <a href="https://www.linkedin.com/in/oshri-el-tzagay-873482226/"><i className="fab fa-linkedin"></i></a> 
+      <a href="mailto:webmaster@example.com"><i className="fas fa-at"></i></a> 
       </div>
-
-      
     </div>
     <div className="cards">
-
-<img src="https://cdn-icons-png.flaticon.com/512/1759/1759366.png" alt="" />
-
+<img src="/Media/matan.jpeg" alt="" />
 <h2>Matan Asayas</h2>
 <div className="contact-icons">
-<GitHubIcon className="icon"/>
-<FacebookRoundedIcon className="icon"/>
-<AlternateEmailRoundedIcon className="icon"/>
-
+<a href="https://github.com/matan-ysayas"><i className="fab fa-github"></i></a>
+<a href="https://www.linkedin.com/in/matan-ysayas-94b105226/"><i className="fab fa-linkedin"></i></a> 
+<a href="mailto:matan.ysayas@gmail.com"><i className="fas fa-at"></i></a> 
 </div>
-
-
 </div>
-
-
   {/* <div class="heart-shape"></div> */}
 
   </div>
   <div className="welcome-bg">
-    <img src="Media/vecteezy_activism_volunteering_illustration-2_hk1220.jpg" alt="" />
+    <img src="Media/vecteezy_activism_volunteering_illustration-2_hk1220.png" alt="" />
 
     <h1 className="iTitle">iVolunteer</h1>
 
