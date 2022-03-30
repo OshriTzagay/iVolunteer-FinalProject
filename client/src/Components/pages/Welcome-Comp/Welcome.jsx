f1cd9a0d6f89b49b2398aadc3c3e9f67f7258a29
OshriTzagay <oshritzagay@gmail.com>
2022-03-30 19:43:19 +0300

    Added PopUp-Logg-off

--- client/src/Components/pages/Welcome-Comp/Welcome.jsx
+++ client/src/Components/pages/Welcome-Comp/Welcome.jsx
@@ -17,6 +17,8 @@ function Welcome() {
     <div className="cards">
       <img src="/Media/lior.jpeg" alt="" />
       <h2>Lior Yosef</h2>
+  <i style={{color:'white'}} class="fas fa-code"></i>
+
       <div className="contact-icons">
       <a href="https://github.com/Lior-Yosef"><i className="fab fa-github"></i></a>
      <a href="https://www.linkedin.com/in/lior-yosef-6314b0225/"><i className="fab fa-linkedin"></i></a> 
@@ -26,6 +28,8 @@ function Welcome() {
     <div className="cards">
       <img src="/Media/oshri.jpg" alt="" />
       <h2>Oshri-el Tzagay</h2>
+<i style={{color:'white'}} class="fas fa-code"></i>
+
       <div className="contact-icons">
       <a href="https://github.com/OshriTzagay"><i className="fab fa-github"></i></a>
       <a href="https://www.linkedin.com/in/oshri-el-tzagay-873482226/"><i className="fab fa-linkedin"></i></a> 
@@ -35,19 +39,19 @@ function Welcome() {
     <div className="cards">
 <img src="/Media/matan.jpeg" alt="" />
 <h2>Matan Asayas</h2>
+ <i style={{color:'white'}} class="fas fa-code"></i>
 <div className="contact-icons">
 <a href="https://github.com/matan-ysayas"><i className="fab fa-github"></i></a>
 <a href="https://www.linkedin.com/in/matan-ysayas-94b105226/"><i className="fab fa-linkedin"></i></a> 
 <a href="mailto:matan.ysayas@gmail.com"><i className="fas fa-at"></i></a> 
 </div>
 </div>
-  {/* <div class="heart-shape"></div> */}
 
   </div>
   <div className="welcome-bg">
     <img src="Media/vecteezy_activism_volunteering_illustration-2_hk1220.png" alt="" />
 
-    <h1 className="iTitle">i Volunteer</h1>
+    <h1 className="iTitle">iVolunteer</h1>
 
 
     <button className="siteBtn" onClick={()=>{navigate('/login')}}>Go-Volunteer!</button>
